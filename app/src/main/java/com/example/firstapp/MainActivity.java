e9f0cb1435a5067310562f456ea9f0c90d26e6bd
Anuththara Chandrasiri <sakchandrasiri@gmail.com>
2020-07-28 05:02:35 +0530

    My FirstApp project will be added to the MADD repository.

---
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.firstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button myFirstButton = findViewById(R.id.btnClickMe);
+
+        myFirstButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                myFirstButton.setText("Hello!");
+            }
+        });
+    }
+}
